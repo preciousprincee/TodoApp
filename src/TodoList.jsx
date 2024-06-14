ac22e6b3fb531682d5b5d2777f2471be1aaf849a
unknown <ppedwards895@gmail.com>
2024-06-14 01:23:41 +0100

    first commit

---
+++ src/TodoList.jsx
@@ -0,0 +1,18 @@
+import { Todoitem } from "./Todoitem"
+
+
+export function TodoList({ todos,toggleTodo, deleteTodo }){
+    return( 
+        <ul className='list'>
+        {todos.length === 0 && "No todos"}
+        {todos.map(todo => {
+            return <Todoitem {...todo} key={todo.id} toggleTodo={toggleTodo} 
+            deleteTodo={deleteTodo}/>
+    
+    
+        })}
+      
+     
+      </ul>
+    )
+}
